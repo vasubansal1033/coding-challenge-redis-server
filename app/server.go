13e71ce4fe52c72b47fc7bd82723e8d5d6c5344c
Vasu Bansal <vasubansal1998@gmail.com>
2024-04-07 23:31:11 +0530

    [Vasu] refactored server.go

--- app/server.go
+++ app/server.go
@@ -2,20 +2,33 @@ package main
 
 import (
 	"fmt"
+	"log"
 	"net"
 	"os"
 )
 
+const (
+	REDIS_HOST = "0.0.0.0"
+	REDIS_PORT = 6379
+)
+
 func main() {
-	l, err := net.Listen("tcp", "0.0.0.0:6379")
+	l, err := net.Listen("tcp", fmt.Sprintf("%s:%d", REDIS_HOST, REDIS_PORT))
 	if err != nil {
-		fmt.Println("Failed to bind to port 6379")
-		os.Exit(1)
+		logAndThrowError(err, fmt.Sprintf("Failed to bind to port: %d", REDIS_PORT))
 	}
+
+	log.Printf("Listening on port: %d", REDIS_PORT)
 
 	_, err = l.Accept()
 	if err != nil {
-		fmt.Println("Error accepting connection: ", err.Error())
-		os.Exit(1)
+		logAndThrowError(err, "Error accepting connection")
 	}
+	
+	log.Printf("Accepted connection. Now listening on port: %d", REDIS_PORT)
+}
+
+func logAndThrowError(err error, errorMessage string) {
+	log.Fatalf("%s: %v", errorMessage, err)
+	os.Exit(1)
 }
